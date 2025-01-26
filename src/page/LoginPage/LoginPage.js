bb2f23544f623e4ddc72e2fca9ee82168eb3cdce
cjy0207 <jiyunjiyun0723@naver.com>
2025-01-26 16:26:44 +0900

    login

--- src/page/LoginPage/LoginPage.js
+++ src/page/LoginPage/LoginPage.js
@@ -1,21 +1,41 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { useNavigate } from "react-router-dom";
+import { GoogleLogin } from "@react-oauth/google";
+import { GoogleOAuthProvider } from "@react-oauth/google";
 import "./style/login.style.css";
+import { clearErrors, loginWithEmail } from "../../features/user/userSlice";
+const GOOGLE_CLIENT_ID = process.env.REACT_APP_GOOGLE_CLIENT_ID;
 
 const Login = () => {
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+  const { user, loginError } = useSelector((state) => state.user);
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
 
+  useEffect(() => {
+    if (loginError) {
+      dispatch(clearErrors());
+    }
+  }, [dispatch, loginError]);
+
   const handleLoginWithEmail = (event) => {
     event.preventDefault();
-    // Implement login logic here
-    console.log("Logging in with", { email, password });
+    dispatch(loginWithEmail({ email, password }));
   };
+
+  if (user) {
+    navigate("/");
+  }
 
   return (
     <div className="login-container">
-      <div className="error-message" style={{ display: "none" }} id="error-message">
-        <div className="alert alert-danger">Login failed. Please try again.</div>
-      </div>
+      {loginError && (
+        <div className="error-message">
+          <div className="alert alert-danger">Login failed. Please try again.</div>
+        </div>
+      )}
       <form className="login-form" onSubmit={handleLoginWithEmail}>
         <div className="form-group">
           <input
@@ -39,11 +59,11 @@ const Login = () => {
           LOG IN
         </button>
 
-          <a href="/register" className="signup-link">
-            SIGN IN
-          </a>
-        
+        <a href="/register" className="signup-link">
+          SIGN IN
+        </a>
       </form>
+      
     </div>
   );
 };
