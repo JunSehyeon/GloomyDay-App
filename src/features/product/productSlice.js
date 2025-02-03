8b01d0c163438faad447dae6294a39dc51065687
cjy0207 <jiyunjiyun0723@naver.com>
2025-02-03 19:59:53 +0900

    landing

--- src/features/product/productSlice.js
+++ src/features/product/productSlice.js
@@ -5,7 +5,15 @@ import { showToastMessage } from "../common/uiSlice";
 // 비동기 액션 생성
 export const getProductList = createAsyncThunk(
   "products/getProductList",
-  async (query, { rejectWithValue }) => {}
+  async (query, { rejectWithValue }) => {
+    try{
+      const response = await api.get("/product")
+      if(response.status!==200) throw new Error(response.error)
+      return response.data.data
+    }catch(error){
+      rejectWithValue(error.error)
+    }
+  }
 );
 
 export const getProductDetail = createAsyncThunk(
@@ -51,7 +59,35 @@ const productSlice = createSlice({
       state.success = false;
     },
   },
-  extraReducers: (builder) => {},
+  extraReducers: (builder) => {
+    builder
+      .addCase(createProduct.pending, (state, action)=>{
+        state.loading=true
+      })
+      .addCase(createProduct.fulfilled, (state, action)=>{
+        state.loading=false
+        state.error=""
+        state.success=true
+      })
+      .addCase(createProduct.rejected, (state, action)=>{
+        state.loading=false
+        state.error=action.payload
+        state.success=false
+      })
+      .addCase(getProductList.pending, (state, action)=>{
+        state.loading=true
+      })
+      .addCase(getProductList.fulfilled, (state, action) => {
+        state.loading = false;
+        state.productList = action.payload
+        state.error = "";
+        //state.totalPageNum = action.payload.totalPageNum
+      })
+      .addCase(getProductList.rejected, (state, action)=>{
+        state.loading=false
+        state.error=action.payload
+      })
+  },
 });
 
 export const { setSelectedProduct, setFilteredList, clearError } =
--- src/page/LandingPage/LandingPage.js
+++ src/page/LandingPage/LandingPage.js
@@ -1,16 +1,77 @@
-import React from "react";
-import "./style/LandingPage.style.css"; // 스타일 파일을 연결합니다.
+import React, { useEffect, useState } from "react";
+import ProductCard from "./components/ProductCard";
+import { Row, Col, Container } from "react-bootstrap";
+import { useSearchParams } from "react-router-dom";
+import { useDispatch, useSelector } from "react-redux";
+import { getProductList } from "../../features/product/productSlice";
+import ReactPaginate from "react-paginate";
+
 
 const LandingPage = () => {
+  const dispatch = useDispatch();
+  const productList = useSelector((state) => state.product.productList);
+  const [query] = useSearchParams();
+  const name = query.get("name");
+  const totalPageNum = useSelector((state) => state.product.totalPageNum); 
+  const [currentPage, setCurrentPage] = useState(1);
+
+  useEffect(() => {
+    dispatch(
+      getProductList({
+        name,
+        page : currentPage,
+      })
+    );
+  }, [query, currentPage]);
+
+
+  const handlePageClick = ({ selected }) => {
+    setCurrentPage(selected + 1); 
+  };
+
   return (
-    <div className="landing-page">
-      <div className="hero">
-        <img
-          src="/image/홈페이지사진.png"
-          className="hero-image"
+    <Container>
+      <Row>
+        {productList.length > 0 ? (
+          productList.map((item) => (
+            <Col md={3} sm={12} key={item._id}>
+              <ProductCard item={item} />
+            </Col>
+          ))
+        ) : (
+          <div className="text-align-center empty-bag">
+            {name === "" ? (
+              <h2>등록된 상품이 없습니다!</h2>
+            ) : (
+              <h2>{name}과 일치한 상품이 없습니다!`</h2>
+            )}
+          </div>
+        )}
+      </Row>
+      <div className="d-flex justify-content-center mt-4">
+        <ReactPaginate
+          nextLabel="next >"
+          onPageChange={handlePageClick}
+          pageRangeDisplayed={5}
+          pageCount={totalPageNum}
+          // forcePage={searchQuery.page - 1}
+          previousLabel="< previous"
+          renderOnZeroPageCount={null}
+          pageClassName="page-item"
+          pageLinkClassName="page-link"
+          previousClassName="page-item"
+          previousLinkClassName="page-link"
+          nextClassName="page-item"
+          nextLinkClassName="page-link"
+          breakLabel="..."
+          breakClassName="page-item"
+          breakLinkClassName="page-link"
+          containerClassName="pagination"
+          activeClassName="active"
+          className="display-center list-style-none"
         />
       </div>
-    </div>
+    </Container>
   );
 };
 
