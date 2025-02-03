7b98380f2e729d4d2ce99e89b09ff2d52fe92bc8
cjy0207 <jiyunjiyun0723@naver.com>
2025-02-03 20:26:52 +0900

    landing2

--- src/page/LandingPage/LandingPage.js
+++ src/page/LandingPage/LandingPage.js
@@ -4,7 +4,8 @@ import { Row, Col, Container } from "react-bootstrap";
 import { useSearchParams } from "react-router-dom";
 import { useDispatch, useSelector } from "react-redux";
 import { getProductList } from "../../features/product/productSlice";
-import ReactPaginate from "react-paginate";
+import ReactPaginate from "react-paginate"; 
+
 
 
 const LandingPage = () => {
